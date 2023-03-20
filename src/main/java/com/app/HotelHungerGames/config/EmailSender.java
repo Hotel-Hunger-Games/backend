0bf235c4584e54114aa4064ce2c15024a643eeb5
Matt <mattwasilewski96@gmail.com>
2023-03-20 22:56:36 +0100

    added email sender

---
+++ src/main/java/com/app/HotelHungerGames/config/EmailSender.java
@@ -0,0 +1,31 @@
+package com.app.HotelHungerGames.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailSender {
+    private final JavaMailSender mailSender;
+
+
+    @Autowired
+    public EmailSender(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendEmail(SimpleMailMessage email) {
+        mailSender.send(email);
+    }
+
+
+    public void sendEmailToWinner(String email) {
+        SimpleMailMessage passwordResetEmail = new SimpleMailMessage();
+        passwordResetEmail.setFrom("support@demo.com");
+        passwordResetEmail.setTo(email);
+        passwordResetEmail.setSubject("Auction in Hotel Hunger Games");
+        passwordResetEmail.setText("Congratulations, you won auction!");
+        sendEmail(passwordResetEmail);
+    }
+}
